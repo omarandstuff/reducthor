8e9471c2756249d54d8757044999653eb6173eae
David De Anda <omarandstuff@gmail.com>
2019-05-19 20:08:11 -0500

    Eslint Typescript config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: 'eslint:recommended',
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
@@ -12,13 +9,5 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module'
   },
-  rules: {},
-  overrides: [
-    {
-      files: 'test/**/*.js',
-      env: {
-        jest: true
-      }
-    }
-  ]
+  rules: {}
 }
