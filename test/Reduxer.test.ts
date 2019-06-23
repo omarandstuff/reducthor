37c82db1985b0737f2da2c63ae3fb734514c16b5
David De Anda <omarandstuff@gmail.com>
2019-06-23 00:55:38 -0500

    Test simple action

--- test/Reduxer.test.ts
+++ test/Reduxer.test.ts
@@ -1,9 +1,23 @@
 import Reduxer from '../src/Reduxer'
+import { Config, Action } from '../src/Reduxer.types'
 
 describe('Reduxer', (): void => {
-  it('creates a store at initializing', (): void => {
-    const reduxer: Reduxer = new Reduxer({nameSpaces: [{name: 'messages'}]})
+  describe('Simple Action', () => {
+    it('Let an action to simply manipulate the state', (): void => {
+      const action: Action = {
+        name: 'SIMPLE_ACTION',
+        action: (state: any) => {
+          return state.set('key', 'value')
+        }
+      }
+      const config: Config = { actions: [action] }
+      const reduxer: Reduxer = new Reduxer(config)
 
-    expect(reduxer.store).not.toBeNull()
+      reduxer.simpleAction()
+
+      const state = reduxer.store.getState()
+
+      expect(state.toJS()).toEqual({ key: 'value' })
+    })
   })
 })
